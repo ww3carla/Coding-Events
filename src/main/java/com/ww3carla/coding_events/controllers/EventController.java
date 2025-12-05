01d13d9c34f16868fb558df6e5f7e9ec44584950
Carla Bozintan <carlabozintan3@gmail.com>
2025-12-05 20:30:45 +0200

    Added basic project structure starter

---
+++ src/main/java/com/ww3carla/coding_events/controllers/EventController.java
@@ -0,0 +1,25 @@
+package com.ww3carla.coding_events.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Controller
+@RequestMapping("events")
+public class EventController {
+
+    @GetMapping
+    public String displayAllEvents(Model model) {
+        List<String> events = new ArrayList<>();
+        events.add("Code With Pride");
+        events.add("Code Contest");
+        events.add("Apple Event");
+        events.add("SpringONE Platform");
+        model.addAttribute("events", events);
+        return "events/index";
+    }
+}
---
+++ src/main/java/com/ww3carla/coding_events/controllers/HomeController.java
@@ -0,0 +1,13 @@
+package com.ww3carla.coding_events.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping
+    public String index() {
+        return "index";
+    }
+}
